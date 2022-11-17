330971ee083cd8a62815fed152deac87c78d0eb0
Pratham Parashar <pratham.parashar143858@gmail.com>
2022-11-17 12:20:17 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+books_db=[
+     {
+        'name':'Secret',
+        'price':275
+    },
+    {
+        'name':'Doop Work',
+        'price':300
+    }
+]
+
+#retrive all the books
+@app.route('/')
+def home():
+    return jsonify({'message':'welcome'})
+
+@app.route('/on')
+def on():
+    return jsonify({'state':'1'})
+
+@app.route('/off')
+def off():
+    return jsonify({'state':'0'})
+
+
+
+
+app.run(port=5500)
